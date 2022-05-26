397f62ca618d6eee30fa165d60f5814530a82c9b
UsagihimeDenka <philippe8910@gmail.com>
2022-05-27 04:43:40 +0800

    added pointer

---
+++ Assets/XRPointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class XRPointer : MonoBehaviour
+{
+    [SerializeField] private float m_defaultLength = 5.0f;
+
+    [SerializeField] private GameObject m_dot;
+
+    [SerializeField] private LineRenderer m_lineRenderer = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        var targetLength = m_defaultLength;
+
+        RaycastHit hit = CreateRaycastHit(targetLength);
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+
+        if (hit.collider)
+        {
+            endPosition = hit.point;
+        }
+
+        m_dot.transform.position = endPosition;
+        
+        m_lineRenderer.SetPosition(0 , transform.position);
+        m_lineRenderer.SetPosition(1 , endPosition);
+    }
+
+    private RaycastHit CreateRaycastHit(float length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        Physics.Raycast(ray , out hit , m_defaultLength);
+
+        return hit;
+    }
+    
+}
