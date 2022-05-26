8d0ccffc5d675116f4cf9d2c568b022b63c3f960
UsagihimeDenka <philippe8910@gmail.com>
2022-05-27 06:49:12 +0800

    added select panel and video player

---
+++ Assets/VideoCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform targetPos;
+    [SerializeField] private Transform mainCamera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(targetPos.position.x , mainCamera.position.y , targetPos.position.z) , 0.1f);
+        transform.LookAt(mainCamera.transform.position);
+    }
+}
