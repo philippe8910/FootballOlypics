71e60c24123b7fede8a016bfceb1b1506296c52f
UsagihimeDenka <philippe8910@gmail.com>
2022-06-01 05:21:23 +0800

    added Subtitle ScriptableObject

---
+++ Assets/Script/Enum/SubtitleData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SubtitleData
+{
+    public List<string> SubtitleList = new List<string>();
+}
+
+
+[CreateAssetMenu(fileName = "New Subtitle" , menuName = "Subtitle/Create Subtitle Asset" , order = 1)]
+public class Subtitle : ScriptableObject
+{
+    public SubtitleData subtitleData;
+
+}
