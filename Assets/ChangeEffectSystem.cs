5eefd2ad07af32fea59563f38a70351601953df0
UsagihimeDenka <philippe8910@gmail.com>
2022-06-04 17:12:42 +0800

    Finish on airplane teleport

---
+++ Assets/ChangeEffectSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project;
+using Project.Event.SubtitleEvent;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class ChangeEffectSystem : MonoBehaviour
+{
+    private Material currentMaterial;
+    
+    void Start()
+    {
+        currentMaterial = GetComponent<MeshRenderer>().material;
+    }
+    
+}
