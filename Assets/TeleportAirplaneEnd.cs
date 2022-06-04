04a985c13223e87bc919acbc561a3ac5285a79e1
UsagihimeDenka <philippe8910@gmail.com>
2022-06-04 18:49:31 +0800

    Player Can teleport to other scnes

---
+++ Assets/TeleportAirplaneEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project;
+using Project.Event;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TeleportAirplaneEnd : MonoBehaviour
+{
+    private TeleportPointXR teleportPointXR;
+    void Start()
+    {
+        teleportPointXR = GetComponent<TeleportPointXR>();
+        teleportPointXR.OnTeleportEnd.AddListener(OnTeleportEnd);
+    }
+
+    private void OnTeleportEnd()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("soccer");
+    }
+}
