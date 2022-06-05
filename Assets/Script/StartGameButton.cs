485104fd00edb08057dff00dbc0c751f9f19c1b6
UsagihimeDenka <philippe8910@gmail.com>
2022-06-05 10:07:14 +0800

    Can Teleport to other Scenes

--- Assets/Script/StartGameButton.cs
+++ Assets/Script/StartGameButton.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Project;
 using Project.Event;
+using Project.Event.SubtitleEvent;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StartGameButton : ButtonEventAdd
 {
-    public override void OnClick()
+    public async override void OnClick()
     {
-        base.OnClick();
-        EventBus.Post(new StartGameDetected());
+        //base.OnClick();
+        EventBus.Post(new ChangeSceneEffectDetected());
+        
+        await Task.Delay(3000);
+        
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 }
