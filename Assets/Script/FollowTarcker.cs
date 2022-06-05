96a1a5e69f31fe618e4d4a4026e4e13aa39c737a
potentialadger <aa00516838@gmail.com>
2022-06-05 13:50:41 +0800

    Fix Some Tourble

---
+++ Assets/Script/FollowTarcker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarcker : MonoBehaviour
+{
+    [SerializeField] Transform trackerTransform;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        transform.position = trackerTransform.position;
+    }
+}
