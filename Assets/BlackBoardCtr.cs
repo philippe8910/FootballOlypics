f401b4956b1dd3f6aa5017826ca20d1e44d80401
potentialadger <aa00516838@gmail.com>
2022-06-09 16:27:16 +0800

    added black board ctr

---
+++ Assets/BlackBoardCtr.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project;
+using  Project.Event;
+
+public class BlackBoardCtr : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> blackBoardUI;
+    void Start()
+    {
+        EventBus.Subscribe<ChangeLevelDetected>(OnChangeLevelDetected);
+        CloseAllBlackBoardUI();
+    }
+
+    private void OnChangeLevelDetected(ChangeLevelDetected obj)
+    {
+        var currentLevel = obj.currentFootLevels;
+        
+        CloseAllBlackBoardUI();
+        OpenBlackBoardUI(currentLevel);
+    }
+
+    private void OpenBlackBoardUI(FootLevels _currentFootLevels)
+    {
+        switch (_currentFootLevels)
+        {
+            case FootLevels.BallSteppingAction:
+                blackBoardUI[0].SetActive(true);
+                break;
+            case FootLevels.InsideRightSideOfSeat:
+                blackBoardUI[1].SetActive(true);
+                break;
+            case FootLevels.OutsideKick:
+                blackBoardUI[1].SetActive(true);
+                break;
+        }
+    }
+
+    private void CloseAllBlackBoardUI()
+    {
+        for (int i = 0; i < blackBoardUI.Count; i++)
+        {
+            blackBoardUI[i].SetActive(false);
+        }   
+    }
+}
---
+++ Assets/ClockTextCtr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockTextCtr : MonoBehaviour
+{
+    [SerializeField] private Text clockText;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
