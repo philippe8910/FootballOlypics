4b1ccf26d8f7fad9538c71031ff702f0608c2a25
potentialadger <aa00516838@gmail.com>
2022-06-09 16:30:50 +0800

    added clock text ctr

--- Assets/ClockTextCtr.cs
+++ Assets/ClockTextCtr.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Project;
+using Project.Event;
 
 public class ClockTextCtr : MonoBehaviour
 {
     [SerializeField] private Text clockText;
+
+    [SerializeField] private int countLimit;
     
     void Start()
     {
-        
+        EventBus.Subscribe<BallSteppingScoreDetedted>(OnBallSteppingScoreDetedted);
     }
 
-    // Update is called once per frame
+    private void OnBallSteppingScoreDetedted(BallSteppingScoreDetedted obj)
+    {
+        var scoreCount = obj.Score;
+
+        clockText.text = scoreCount + "/" + countLimit;
+    }
+
     void Update()
     {
         
