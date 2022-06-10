20d82f5e75f401d3a0f9964bca42f846459db463
UsagihimeDenka <philippe8910@gmail.com>
2022-06-10 12:53:24 +0800

    added new Skin box and animator

--- Assets/SkinBox.cs
+++ Assets/SkinBox.cs
@@ -8,13 +8,24 @@ using UnityEngine;
 public class SkinBox : ButtonXR
 {
     [SerializeField] private Material _material;
+
+    [SerializeField] private Animator animator;
     private void Start()
     {
+        animator = GetComponent<Animator>();
+        
         ClickAction.AddListener(Trigger);
+        EventBus.Subscribe<ChangeSkinDetected>(OnChangeSkinDetected);
+    }
+
+    private void OnChangeSkinDetected(ChangeSkinDetected obj)
+    {
+        animator.Play("BoxIdle");
     }
 
     private void Trigger()
     {
         EventBus.Post(new ChangeSkinDetected(_material));
+        animator.Play("BoxOpen");
     }
 }
